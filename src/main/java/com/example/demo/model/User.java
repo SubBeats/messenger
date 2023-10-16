f3b96262266ca82bc4a00a45f2da7217d028dda7
sabit <sabitovbulat0@gmail.com>
2023-10-16 22:03:35 +0300

    make user model

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,37 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Entity;
+import org.hibernate.annotations.Table;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(appliesTo = "m_user")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Size(min = 0,message = "Имя не может быть пустым")
+    private String firstName;
+    @Size(min = 0,message = "Фамилия не может быть пустым")
+    private String secondName;
+    private String lastNAme;
+    @Size(min=5, message = "Не меньше 5 знаков")
+    private String password;
+    @Transient
+    private String passwordConfirm;
+    private boolean is_actual;
+
+}
